1e12dd7c6c966421d58f8d8faf4e6ccf8e4afc05
Stéphane Graber <stgraber@ubuntu.com>
2012-06-13 13:17:10 -0400

    Update test code to check both python2 and python3

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 teststring="blah blah blah"
 
-for pastebin in $(./pastebinit -l | egrep "^-" | sed "s/^- //g")
+for interpreter in python python3
 do
-    echo "Trying http://$pastebin"
-    URL=$(echo -e "$teststring\n$teststring\n$teststring" | ./pastebinit -b http://$pastebin)
-    out=$(wget -O - -q "$URL" | grep "$teststring")
-    if [ -n "$out" ]; then
-        echo "PASS: http://$pastebin ($URL)"
-    else
-        echo "FAIL: http://$pastebin ($URL)"
-    fi
+    for pastebin in $(./pastebinit -l | egrep "^-" | sed "s/^- //g")
+    do
+        echo "Trying http://$pastebin ($interpreter)"
+        URL=$(echo -e "$teststring\n$teststring\n$teststring" | $interpreter pastebinit -b http://$pastebin)
+        out=$(wget -O - -q "$URL" | grep "$teststring")
+        if [ -n "$out" ]; then
+            echo "PASS: http://$pastebin ($URL) ($interpreter)"
+        else
+            echo "FAIL: http://$pastebin ($URL) ($interpreter)"
+        fi
+        echo ""
+    done
 done
