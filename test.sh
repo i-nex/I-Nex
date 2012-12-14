38e4ae812cc3e0a821fc7591f2604b31d4c782de
eloaders <eloaders@yahoo.com>
2012-12-14 11:25:53 +0100

    Downgrade pastebinit script Update nopaste services

--- test.sh
+++ test.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 teststring="blah blah blah"
 
-for interpreter in python python3
+for pastebin in $(./pastebinit -l | egrep "^-" | sed "s/^- //g")
 do
-    for pastebin in $(./pastebinit -l | egrep "^-" | sed "s/^- //g")
-    do
-        echo "Trying http://$pastebin ($interpreter)"
-        URL=$(echo -e "$teststring\n$teststring\n$teststring" | $interpreter pastebinit -b http://$pastebin)
-        out=$(wget -O - -q "$URL" | grep "$teststring")
-        if [ -n "$out" ]; then
-            echo "PASS: http://$pastebin ($URL) ($interpreter)"
-        else
-            echo "FAIL: http://$pastebin ($URL) ($interpreter)"
-        fi
-        echo ""
-    done
+    echo "Trying http://$pastebin"
+    URL=$(echo -e "$teststring\n$teststring\n$teststring" | ./pastebinit -b http://$pastebin)
+    out=$(wget -O - -q "$URL" | grep "$teststring")
+    if [ -n "$out" ]; then
+        echo "PASS: http://$pastebin ($URL)"
+    else
+        echo "FAIL: http://$pastebin ($URL)"
+    fi
 done
